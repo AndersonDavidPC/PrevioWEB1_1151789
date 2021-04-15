750741733d988039857b315656aac9291b3a27f4
Anderson Patiño <andersondavidpc@ufps.edu.co>
2021-04-15 07:46:01 -0500

    Js Cartas

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const tabla = document.getElementById('tabla')
+function cargarTabla() {
+  var url ="data.json"
+  fetch(url)
+    .then(response=>response.json)
+    .then(data=>generarTabla(data))
+  .catch(error=>{})
+}
+function generarTabla(json) {
+  for (var i in json.cartas) {
+    const tr=document.createElement('tr')
+    const tdNumero=document.createElement('td')
+      tdNumero.textContent=json.cartas[i].numero
+      tr.appendChild(tdNumero)
+    const tr=document.createElement('tr')
+    const tdCarta=document.createElement('td')
+      tdCarta.textContent=json.cartas[i].carta
+      tr.appendChild(tdCarta)
+    const tr=document.createElement('tr')
+    const tdCant=document.createElement('td')
+      tdCant.textContent=json.cartas[i].cant
+      tr.appendChild(tdCant)
+  tabla.tBodies[0].appendChild(tr)
+  }
+
+}
+cargarTabla()
